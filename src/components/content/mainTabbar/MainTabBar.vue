af6372c86a33fe346ea81a56bb03559949ad38f8
Dmyis <1514378456@qq.com>
2020-02-01 12:08:03 +0800

    test

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -2,23 +2,23 @@
   <tab-bar>
       <tab-bar-item path='/home'>
       <!-- html代码使用别名前面要加上~ -->
-        <img slot="item-icon" src="assets/img/tabbar/home.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/home_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item path='/category'>
-        <img slot="item-icon" src="assets/img/tabbar/category.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/category-active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item path='/cart'>
-        <img slot="item-icon" src="assets/img/tabbar/shopcart.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/shopcart-active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item path='/proflie'>
-        <img slot="item-icon" src="assets/img/tabbar/proflie.svg" alt="">
-        <img slot="item-icon-active" src="assets/img/tabbar/proflie-active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
